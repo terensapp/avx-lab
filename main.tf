0d14e209cd9556dfcd665312a2c2a86c8f300d18
Teren Sapp <terensapp@gmail.com>
2021-11-01 09:30:12 -0500

    Adding HA functionality

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "aws_spoke" {
   name            = "${each.key}"
   cidr            = "${lookup(each.value, "cidr")}"
   instance_size   = var.aws_spoke_instance_size
-  ha_gw           = try(${lookup(each.value, "ha_enabled")}, false)
+  ha_gw           = try("${lookup(each.value, "ha_enabled")}", false)
   prefix          = false
   suffix          = false
   transit_gw      = "${lookup(each.value, "transit")}"
