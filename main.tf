19f8c38ce4a88fb0b9cc2f71ef051d4d691c240a
Teren Sapp <terensapp@gmail.com>
2021-11-01 09:47:30 -0500

    Adding HA functionality

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "aws_transit" {
   region              = "${lookup(each.value, "region")}"
   name                = "${each.key}"
   cidr                = "${lookup(each.value, "cidr")}"
-  ha_gw               = "${lookup(each.value, "ha_enabled", false)}"
+  ha_gw               = tobool("${lookup(each.value, "ha_enabled", false)}")
   prefix              = false
   instance_size       = var.aws_transit_instance_size
   enable_segmentation = true
@@ -23,7 +23,7 @@ module "aws_spoke" {
   name            = "${each.key}"
   cidr            = "${lookup(each.value, "cidr")}"
   instance_size   = var.aws_spoke_instance_size
-  ha_gw           = "${lookup(each.value, "ha_enabled", false)}"
+  ha_gw           = tobool("${lookup(each.value, "ha_enabled", false)}")
   prefix          = false
   suffix          = false
   transit_gw      = "${lookup(each.value, "transit")}"
