e9f8217ce52d3b61faa4f85dbea5096a22856ffa
Teren Sapp <terensapp@gmail.com>
2021-11-02 16:27:10 -0500

    Adding test hosts

--- aws.tf
+++ aws.tf
@@ -41,6 +41,7 @@ module "security_group_hosts" {
     ingress_cidr_blocks = ["0.0.0.0/0"]
     ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
     egress_rules        = ["all-all"]
+    region              = "${lookup(each.value, "region")}"
 
     depends_on = [module.aws_transit, module.aws_spoke]
 }
@@ -58,6 +59,7 @@ module "aws_spoke_hosts" {
     vpc_security_group_ids      = [module.security_group_hosts["${each.key}"].this_security_group_id]
     associate_public_ip_address = true
     user_data_base64            = base64encode(local.host_user_data)
+    region                      = "${lookup(each.value, "region")}"
 
     depends_on = [module.aws_transit, module.aws_spoke, module.security_group_hosts]
 }
