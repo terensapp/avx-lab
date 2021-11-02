35e951cf258170394e3d5dcef62c56b29774384b
Teren Sapp <terensapp@gmail.com>
2021-11-02 11:41:07 -0500

    Testing new loop

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ module "aws_transit" {
   for_each = var.gateways.transit
   source              = "terraform-aviatrix-modules/aws-transit/aviatrix"
   version             = "4.0.1"
-  account             = "${lookup(each.value, "account")}"
-  region              = "${lookup(each.value, "region")}"
-  name                = "${each.key}"
-  cidr                = "${lookup(each.value, "cidr")}"
-  ha_gw               = coalesce("${lookup(each.value, "ha_enabled")}",false)
+  account             = each.value.account
+  region              = each.value.region
+  name                = each.key
+  cidr                = each.value.cidr
+  ha_gw               = coalesce(each.value.ha_enabled,false)
   prefix              = false
   suffix              = false
   instance_size       = var.aws_transit_instance_size
@@ -34,8 +34,8 @@ module "aws_spoke" {
 
 resource "aviatrix_spoke_transit_attachment" "test_attachment" {
   for_each = var.gateways.spoke
-  spoke_gw_name   = "${each.key}"
-  transit_gw_name = "${lookup(each.value, "transit")}"
+  spoke_gw_name   = each.key
+  transit_gw_name = each.value.transit
 
   depends_on = [module.aws_transit, module.aws_spoke]
 }
