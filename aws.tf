9b0aa3256908141c3c49c6b4c129caaa19d32b88
Teren Sapp <terensapp@gmail.com>
2021-11-02 13:04:55 -0500

    Adding test hosts

--- aws.tf
+++ aws.tf
@@ -31,19 +31,19 @@ EOF
 }
 
 module "security_group_hosts" {
-  for_each =  {for key, value in var.gateways.spoke: key => value if coalesce(value.attach_host,false)}
+  for_each =  {for key, value in var.gateways.spoke: key => value if value.attach_host == coalesce(value.attach_host,false)}
   
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "~> 3.0"
-  name                = "test_host_sg"
-  description         = "Security group for example usage with EC2 instance"
-  vpc_id              = module.aws_spoke["${each.key}"].vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-  providers = {
-    aws = aws.ohio
-  }
+    source              = "terraform-aws-modules/security-group/aws"
+    version             = "~> 3.0"
+    name                = "test_host_sg"
+    description         = "Security group for example usage with EC2 instance"
+    vpc_id              = module.aws_spoke["${each.key}"].vpc.vpc_id
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+    ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
+    egress_rules        = ["all-all"]
+    providers = {
+      aws = aws.ohio
+    }
 }
 
 #module "aws_spoke1_bastion" {
