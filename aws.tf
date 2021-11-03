9d24419f5654b477ab943477c7f4c53c84364c6e
Teren Sapp <terensapp@gmail.com>
2021-11-03 13:20:32 -0500

    Adding test hosts

--- aws.tf
+++ aws.tf
@@ -43,8 +43,7 @@ resource "aws_security_group" "allow_ssh" {
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+      cidr_blocks      = ["0.0.0.0/0"]
     }
   ]
 
@@ -54,7 +53,6 @@ resource "aws_security_group" "allow_ssh" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 }
