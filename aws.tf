24af3ed694149325844950299c8a7af612680339
Teren Sapp <terensapp@gmail.com>
2021-11-03 10:25:22 -0500

    Adding test hosts

--- aws.tf
+++ aws.tf
@@ -30,41 +30,41 @@ sudo echo "<html><h1>Aviatrix is awesome</h1></html>" > /var/www/html/index.html
 EOF
 }
 
-region = "us-east-2"
+provider = {
+  region = "us-east-2"
+}
 
 module "security_group_hosts" {
   for_each =  {for key, value in var.gateways.spoke: key => value if coalesce(value.attach_host,false)}
   
-    source = "terraform-aws-modules/security-group/aws//modules/http-80"
-
-    name        = "web-server"
-    description = "Security group for web-server with HTTP ports open within VPC"
-    vpc_id      = module.aws_spoke["${each.key}"].vpc.vpc_id
-
-    ingress_cidr_blocks = ["10.10.0.0/16"]
+    source              = "terraform-aws-modules/security-group/aws"
+    version             = "~> 3.0"
+    name                = "test_host_sg"
+    description         = "Security group for example usage with EC2 instance"
+    vpc_id              = module.aws_spoke["${each.key}"].vpc.vpc_id
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+    ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
+    egress_rules        = ["all-all"]
+    #region              = "${lookup(each.value, "region")}"
 }
 
-#module "aws_spoke_hosts" {
-#  for_each =  {for key, value in var.gateways.spoke: key => value if coalesce(value.attach_host,false)}
-#    source                      = "terraform-aws-modules/ec2-instance/aws"
-#    version                     = "2.21.0"
-#    instance_type               = var.aws_test_instance_size
-#    name                        = "${each.key}-host"
-#    ami                         = data.aws_ami.ubuntu.id
-#    key_name                    = var.ec2_key_name
-#    instance_count              = 1
-#    subnet_id                   = module.aws_spoke["${each.key}"].vpc.public_subnets[0].subnet_id
-#    vpc_security_group_ids      = [module.security_group_hosts["${each.key}"].this_security_group_id]
-#    associate_public_ip_address = true
-#    user_data_base64            = base64encode(local.host_user_data)
+module "aws_spoke_hosts" {
+  for_each =  {for key, value in var.gateways.spoke: key => value if coalesce(value.attach_host,false)}
+    source                      = "terraform-aws-modules/ec2-instance/aws"
+    version                     = "2.21.0"
+    instance_type               = var.aws_test_instance_size
+    name                        = "${each.key}-host"
+    ami                         = data.aws_ami.ubuntu.id
+    key_name                    = var.ec2_key_name
+    instance_count              = 1
+    subnet_id                   = module.aws_spoke["${each.key}"].vpc.public_subnets[0].subnet_id
+    vpc_security_group_ids      = [module.security_group_hosts["${each.key}"].this_security_group_id]
+    associate_public_ip_address = true
+    user_data_base64            = base64encode(local.host_user_data)
     #region                      = "${lookup(each.value, "region")}"
-#    providers = {
-#      region = "${lookup(each.value, "region")}"
-#    }
 
-#    depends_on = [module.aws_transit, module.aws_spoke, module.security_group_hosts]
-#}
-
+    depends_on = [module.aws_transit, module.aws_spoke, module.security_group_hosts]
+}
 
 #output "aws_spoke1_bastion_public_ip" {
 #  value = module.aws_spoke1_bastion.public_ip
