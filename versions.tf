b3edf5ab3da45985b659a62963d22d885fdd84b1
Teren Sapp <terensapp@gmail.com>
2021-11-03 13:25:34 -0500

    Adding test hosts

--- versions.tf
+++ versions.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.20"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
     }
   }
   required_version = ">= 1.0"
