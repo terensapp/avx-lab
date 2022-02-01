98007f56b41a246c71a5875bc167c058869695dc
Teren Sapp <terensapp@gmail.com>
2022-01-31 20:37:47 -0600

    First Try

--- main.tf
+++ main.tf
@@ -12,3 +12,16 @@ module "spokes" {
     account          = each.value.account
     attached         = false
 }
+
+module "transit" {
+  for_each =  {for key, value in var.gateways.transit: key => value}
+    source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
+    version = "1.0.1"
+
+    cloud         = each.value.cloud
+    name             = each.key
+    region        = each.value.region
+    cidr          = each.value.cidr
+    account       = each.value.account
+    ha_gw         = false
+}
