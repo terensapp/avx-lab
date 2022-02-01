610da057df46c791dc769e7529d1c323bd7780b8
Teren Sapp <terensapp@gmail.com>
2022-01-31 19:47:48 -0600

    First Try

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@
 
 module "spokes" {
   for_each =  {for key, value in var.gateways.spoke: key => value}
-  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
-  version = "1.1.0"
+    source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+    version = "1.1.0"
 
-  cloud            = each.value.cloud
-  name             = each.key
-  region           = each.value.region
-  cidr             = each.value.cidr
-  account          = each.value.region
-  attached         = false
+    cloud            = each.value.cloud
+    name             = each.key
+    region           = each.value.region
+    cidr             = each.value.cidr
+    account          = each.value.region
+    attached         = false
 }
